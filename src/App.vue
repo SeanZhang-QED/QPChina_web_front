049e3a6a7920be613fe07bbf3b71361532c3d056
SeanZhang-QED <66594541+SeanZhang-QED@users.noreply.github.com>
2022-05-04 17:14:00 -0700

    Added Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <Navbar />
+    </header>
+    <body>
+    </body>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'monterrat', sans-serif;
+}
+
+header {
+  width: 100vw;
+  background-color: black;
+  padding: 15px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav>
+        <div class="menu-item"><a href="#">Home</a></div>
+        <div class="menu-item"><a href="#">Event</a></div>
+        <div class="menu-item"><a href="#">About us</a></div>
+        <div class="menu-item"><a href="#">Blogs</a></div>
+        <div class="menu-item"><a href="#">Governance</a></div>
+        <div class="menu-item"><a href="#">Opportunities</a></div>
+        <div class="menu-item"><a href="#">Support Us</a></div>
+        <div class="menu-item"><a href="#">Follow Us</a></div>
+        <div class="menu-item donation"> </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name:'Nav-bar'
+}
+</script>
+
+<style>
+nav {
+    display:flex;
+    align-items: center;
+    justify-content: center;
+}
+
+nav .menu-item {
+    color: #F7CBD8;
+    padding: 8px 20px;
+    position: relative;
+    text-align: center;
+    border-bottom: 3px solid transparent;
+    display: flex;
+    transition: 0.4s;
+} 
+
+nav .menu-item a.active,
+nav .menu-item a:hover {
+    color: #F5A6BD;
+}
+
+nav .menu-item a {
+    color: inherit;
+    font-weight: bold;
+    text-decoration: none;
+    white-space: nowrap;
+}
+</style>
